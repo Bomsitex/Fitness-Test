ce073e32fed18e6392d45ac1a33c55db2af0d826
Bomsitex <107302343+Bomsitex@users.noreply.github.com>
2022-11-22 13:01:06 +0100

    Fitness Test
    HerTechTrail Cohort 5
    Frontend Developer Pro

---
+++ HerTechTrail- Web_Dev_Pro/script.js
@@ -0,0 +1,41 @@
+const character1 = document.getElementById("mickey");
+
+character1.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+});
+
+const character2 = document.getElementById("dora");
+
+character2.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "pink";
+});
+
+const character3 = document.getElementById("winnie");
+
+character3.addEventListener("click", (event) => {
+    event.target.style.background = "gold";
+    event.target.style.color = "red";
+});
+
+const character4 = document.getElementById("superman");
+
+character4.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "red";
+});
+
+const character5 = document.getElementById("scoobydoo");
+
+character5.addEventListener("click", (event) => {
+    event.target.style.background = "grey";
+    event.target.style.color = "white";
+});
+
+const character6 = document.getElementById("tomjerry");
+
+character6.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "gold";
+});
